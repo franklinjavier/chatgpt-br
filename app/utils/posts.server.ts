f48c8f922f7e1341352ffbda5f8c8a4acbc80f91
Franklin Javier <franklinjalves@gmail.com>
2022-12-06 17:30:10 +0000

    fix: 🐛 add posts utils

---
+++ app/utils/posts.server.ts
@@ -0,0 +1,46 @@
+import { getImageMD, regexMDImage } from './get-image-from-markdown.server'
+import { markdownToHtml } from './markdown.server'
+import { slugify } from './slugify'
+import { supabase } from './supabase.server'
+
+export type Post = {
+  slug: string
+  body: string
+  excerpt: string
+  created_at: string
+  updated_at: string
+  title: string
+  id: number
+  labels: [
+    {
+      id: string
+      label: string
+    },
+  ]
+}
+
+export async function getPosts() {
+  const { data } = await supabase.from('posts').select()
+  const result = data ?? []
+
+  const posts = await Promise.all(
+    result.map(async ({ body: content, ...rest }: Post) => {
+      const body = await markdownToHtml(content.replace(regexMDImage, ''))
+      const image = getImageMD(body)
+      const slug = slugify(rest.title)
+      const excerpt = `${body.split('.')[0]}.`
+
+      console.log(rest)
+
+      return {
+        ...rest,
+        image,
+        slug,
+        excerpt,
+        body,
+      }
+    }),
+  )
+
+  return posts
+}
