eb3d49f7c0a7924d8f5dc04e6701c7d021561a4f
Franklin Javier <franklinjalves@gmail.com>
2022-12-07 19:08:20 +0000

    refactor: 💡 new ga code

--- app/components/analytics/Analytics.tsx
+++ app/components/analytics/Analytics.tsx
@@ -1,13 +1,13 @@
 export function Analytics() {
   return (
     <>
-      <script src="https://www.googletagmanager.com/gtag/js?id=G-8KH9L5MZKT" async></script>
+      <script src="https://www.googletagmanager.com/gtag/js?id=G-Q6TC4N8JKC" async></script>
       <script
         dangerouslySetInnerHTML={{
           __html: `
           window.dataLayer = window.dataLayer || [];
           function gtag(){window?.dataLayer?.push(arguments);}
-            gtag('js', new Date()); gtag('config', 'G-8KH9L5MZKT');`,
+            gtag('js', new Date()); gtag('config', 'G-Q6TC4N8JKC');`,
         }}
       />
     </>
