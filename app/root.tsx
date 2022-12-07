1986486f83800042da85aa9ea7864f7bd6100bc9
Franklin Javier <franklinjalves@gmail.com>
2022-12-07 13:09:27 +0000

    feat: 🎸 ga script

--- app/root.tsx
+++ app/root.tsx
@@ -68,6 +68,18 @@ const Document = ({ children }: DocumentProps) => {
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
+        <script src="https://www.googletagmanager.com/gtag/js?id=G-8KH9L5MZKT" async></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+          function gtag(){window?.dataLayer?.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-8KH9L5MZKT');
+          `,
+          }}
+          id="gtag"
+        />
       </body>
     </html>
   )
